fccb70bbf3b7a2b0a75331ed31313222ed83edc1
Monil Gangar <monil.gangar@optum.com>
2021-02-22 16:22:28 -0600

    First commit - regarding Stateful components and props and states

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Button, Typography } from "@material-ui/core";
+import Confirm from "./components/confirm";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface IState {
+  confirmOpen: boolean;
+  confirmMessage: string;
+}
+
+class App extends Component<{}, IState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      confirmOpen: false,
+      confirmMessage: "Please hit the confirm button",
+    };
+  }
+
+  public render() {
+    return (
+      <>
+        <div className="App">
+          <p>{this.state.confirmMessage}</p>
+          <button onClick={this.handleConfirmClick}>Confirm</button>
+          <Confirm
+            open={this.state.confirmOpen}
+            title="React and TypeScript"
+            content="Are you sure you want to learn React and TypeScript?"
+            cancelCaption="No way"
+            okCaption="Yes please!"
+            onCancelClick={this.handleCancelConfirmClick}
+            onOkClick={this.handleOkConfirmClick}
+          />
+        </div>
+      </>
+    );
+  }
+
+  private handleConfirmClick = () => {
+    this.setState({ confirmOpen: true });
+  };
+
+  private handleCancelConfirmClick = () => {
+    this.setState({
+      confirmMessage: "Take a break, I'm sure you will later ...",
+      confirmOpen: false,
+    });
+  };
+
+  private handleOkConfirmClick = () => {
+    this.setState({
+      confirmMessage: "Cool, carry on reading!",
+      confirmOpen: false,
+    });
+  };
 }
 
 export default App;
